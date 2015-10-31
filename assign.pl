2a54a788fb35e83cadd3b20f740f21cad01a1faa
Alexander Ivanov <alehander42@gmail.com>
2015-10-31 15:20:22 +0200

    Add an initial translate implementation

---
+++ assign.pl
@@ -0,0 +1,3 @@
+package K;
+
+my $s = 2;
---
+++ lib/Pseudon/Translator.pm
@@ -0,0 +1,13 @@
+package Pseudon::Translator;
+use strict;
+use warnings;
+
+use Exporter qw(import);
+
+our @EXPORT_OK = qw(translate);
+
+sub translate {
+  my ($source) = @_;
+
+  return "(Cell)\n";
+}
